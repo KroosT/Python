a25fb7d9ceaa2326fc29e31485b2cb1c46b903cd
KroosT <anton.shestak.96@gmail.com>
2016-04-24 21:07:07 +0300

    @cached added

---
+++ Python/Lab_002/8.decorator @cached/cached.py
@@ -0,0 +1,35 @@
+def cached(function):
+
+    def wrapper(*args):
+
+        wrapper.previous_args = ""
+        try:
+            wrapper.count += 1
+        except AttributeError:
+            wrapper.count = 1
+            wrapper.dict = {}
+
+        for i in range(len(args)):
+            wrapper.previous_args += str(args[i])
+        if wrapper.previous_args in wrapper.dict:
+            return wrapper.dict[wrapper.previous_args]
+        else:
+            result = function(*args)
+            wrapper.dict[wrapper.previous_args] = result
+            return result
+    return wrapper
+
+
+@cached
+def union(*args):
+    string = "{"
+    for i in range(len(args)):
+        string += str(args[i])
+        if i != len(args) - 1:
+            string += ", "
+    string += "}"
+    return string
+
+print union(0, 1, 3, [1, 3])
+print union(0, 1, 3, [1, 3])
+print union(0, 1, 2)
