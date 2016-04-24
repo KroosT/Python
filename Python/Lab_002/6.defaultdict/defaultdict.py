bc69170de02ced72f860e63905a450ef67020eb9
KroosT <anton.shestak.96@gmail.com>
2016-04-24 20:14:29 +0300

    DefaultDict added

---
+++ Python/Lab_002/6.defaultdict/defaultdict.py
@@ -0,0 +1,15 @@
+class DefaultDict(dict):
+    def __getitem__(self, item):
+        try:
+            return super(DefaultDict, self).__getitem__(item)
+        except KeyError:
+            self[item] = DefaultDict()
+            return self[item]
+
+x = DefaultDict()
+x["4:{3:5}"][4][3] = 5
+x[10][1] = 9
+x[10][2] = 4
+x[10][3] = "a"
+x[4] = "b"
+print x
