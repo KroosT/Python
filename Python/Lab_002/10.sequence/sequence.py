504e8a93f0670f82d63676ee64d07e14769cde68
KroosT <anton.shestak.96@gmail.com>
2016-04-24 21:25:00 +0300

    sequence with deselection added

---
+++ Python/Lab_002/10.sequence/sequence.py
@@ -0,0 +1,28 @@
+class Filter(object):
+
+    def __init__(self, args):
+        self.object = [elem for elem in args]
+
+    def __iter__(self):
+        return iter(self.object)
+
+    def __str__(self):
+        return str(self.object)
+
+    def deselect(self, function):
+        for i in range(len(self.object)):
+            if function(self.object[i]):
+                yield self.object[i]
+            else:
+                pass
+
+
+def even(number):
+
+    if number % 2 == 0:
+        return True
+    else:
+        return False
+
+a = Filter([5, 1, 2, 3, 4])
+print list(a.deselect(even))
